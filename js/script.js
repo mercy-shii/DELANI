59aeacf3a30cf7b9f42c0edfa807d8d4b1d16e2e
mercy-shii <wemercy525@gmail.com>
2020-02-26 11:41:27 +0300

    Delani

--- js/script.js
+++ js/script.js
@@ -34,4 +34,70 @@ $(document).ready(function(){
 
 
 
+    $(document).ready(function(){
+        $("#work3").hover(function(){
+            $("#test3").fadeIn("slow");
+            $("#test3").show();
+        }).mouseout(function(){
+            $("#test3").hide();
+        });
+        });
+
+
+
+    $(document).ready(function(){
+        $("#work2").hover(function(){
+            $("#test2").fadeIn("slow");
+            $("#test2").show();
+        }).mouseout(function(){
+            $("#test2").hide();
+        });
+        });
+
+
+
+    $(document).ready(function(){
+        $("#work1").hover(function(){
+            $("#test1").fadeIn("slow");
+            $("#test1").show();
+        }).mouseout(function(){
+            $("#test1").hide();
+        });
+        });
+
+
+
+    $(document).ready(function(){
+        $("#work5").hover(function(){
+            $("#test5").fadeIn("slow");
+            $("#test5").show();
+        }).mouseout(function(){
+            $("#test5").hide();
+        });
+        });
+
+
+
+    $(document).ready(function(){
+        $("#work6").hover(function(){
+            $("#test6").fadeIn("slow");
+            $("#test6").show();
+        }).mouseout(function(){
+            $("#test6").hide();
+        });
+        });
+
+
+
+    $(document).ready(function(){
+        $("#work7").hover(function(){
+            $("#test7").fadeIn("slow");
+            $("#test7").show();
+        }).mouseout(function(){
+            $("#test7").hide();
+        });
+        });
+
+
+
 });
