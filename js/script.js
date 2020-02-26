0c846d7e52d0ea47e0bf179a9e1b20d9eff286fa
mercy-shii <wemercy525@gmail.com>
2020-02-26 11:33:30 +0300

    Delani

--- js/script.js
+++ js/script.js
@@ -10,14 +10,10 @@ $(document).ready(function(){
     $("#img3").click(function(){
         $("#product").toggle()
     });
-
-    
-    
-
      $(".submit").click(function(){
-        var name =  $(".name").val()
-        var Email =  $(".Email").val()
-        var message =$(".message").val()
+        var name =  $("mce-FNAME").val()
+        var Email =  $("mce-EMAIL").val()
+        var  message=$("mce-MESSAGE").val()
 
         if(name && Email && message){
             alert(name + " we have received your message.Thank you for reaching to us");
@@ -25,9 +21,15 @@ $(document).ready(function(){
         }else{
             alert('check your input');
         }
+    });
 
-        
-
+    $(document).ready(function(){
+    $("#work4").hover(function(){
+        $("#test4").fadeIn("slow");
+        $("#test4").show();
+    }).mouseout(function(){
+        $("#test4").hide();
+    });
     });
 
 
